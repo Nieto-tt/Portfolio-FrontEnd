91c7efb02f1f729cad539ec5950be17c2fa49821
Tomas Nieto <tominieto2004@gmail.com>
2023-05-14 03:39:22 -0300

    Arquitectura del proyecto completa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,33 @@ import { AppComponent } from './app.component';
 import { AboutComponent } from './components/about/about.component';
 import { ContactComponent } from './components/contact/contact.component';
 import { ErrorComponent } from './components/error/error.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { ExperienceComponent } from './components/experience/experience.component';
+import { EducationComponent } from './components/education/education.component';
+import { SkillsComponent } from './components/skills/skills.component';
+import { LoginComponent } from './components/login/login.component';
+import { CertificationComponent } from './components/certification/certification.component';
+import { PortfolioComponent } from './components/portfolio/portfolio.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AboutComponent,
     ContactComponent,
-    ErrorComponent
+    ErrorComponent,
+    NavbarComponent,
+    HeaderComponent,
+    FooterComponent,
+    ProjectsComponent,
+    ExperienceComponent,
+    EducationComponent,
+    SkillsComponent,
+    LoginComponent,
+    CertificationComponent,
+    PortfolioComponent
   ],
   imports: [
     BrowserModule,
