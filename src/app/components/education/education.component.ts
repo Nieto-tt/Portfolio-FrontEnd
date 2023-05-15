27f8f95894e1dec25b59feb0a931a49d1e442756
Tomas Nieto <tominieto2004@gmail.com>
2023-05-15 16:06:59 -0300

    componente education finalizado

--- src/app/components/education/education.component.ts
+++ src/app/components/education/education.component.ts
@@ -1,10 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { EducationDTO } from 'src/app/model/educationDTO';
+import { PortfolioService } from 'src/app/services/portfolio.service';
 
 @Component({
   selector: 'app-education',
   templateUrl: './education.component.html',
   styleUrls: ['./education.component.css']
 })
-export class EducationComponent {
+export class EducationComponent implements OnInit{
+
+  @Input() isLogged!: boolean;
+  @Input() userId! : number;
+  @Input() listEdu: EducationDTO[] = [];
+  @Output() recargandoPortfolio = new EventEmitter<any>();
+
+  education!: EducationDTO;
+  tituloModal: string = "";
+  Edit: boolean = false;
+
+  constructor(private portfolioServ : PortfolioService){}
+  
+  ngOnInit(): void {
+
+  }
+
+  openModal(){
+    let edu = {id:0,userId:this.userId, institutionName:"",degreeName:"",fieldOfStudy:"",startDate:"",endDate:"",imageUrl:""};
+    this.education = edu;
+    this.tituloModal = "Agregar elemento a Educación";
+    this.Edit = true;
+  }
+
+  openEdit(edu: EducationDTO){
+    this.education = edu;
+    this.tituloModal = "Editar elemento en Educación";
+    this.Edit = true;
+  }
+
+  deleteEducation(eduId:any){
+    if(eduId != undefined && confirm("¿Estás segura de querer eliminar este elemento?")){
+      this.portfolioServ.deleteEducation(eduId).subscribe(data => {
+        alert("¡Educación eliminada con éxito!");
+        this.recargandoPortfolio.emit();
+        });
+    }
+  }
+
+  closeModal(){
+    this.Edit = false;
+    this.recargandoPortfolio.emit();
+  }
+
 
 }
--- src/app/components/skills/skills.component.ts
+++ src/app/components/skills/skills.component.ts
@@ -1,10 +1,86 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { SkillsDTO } from 'src/app/model/skillsDTO';
+import { softskillsDTO } from 'src/app/model/softskillsDTO';
+import { PortfolioService } from 'src/app/services/portfolio.service';
 
 @Component({
   selector: 'app-skills',
   templateUrl: './skills.component.html',
   styleUrls: ['./skills.component.css']
 })
-export class SkillsComponent {
+export class SkillsComponent implements OnInit{
+  @Input() isLogged!: boolean;
+  @Input() userId! : number;
+  @Input() listSkill: SkillsDTO[] = [];
+  @Input() listSoft: softskillsDTO[] = [];
+  @Output() recargandoPortfolio = new EventEmitter<any>();
 
+  skill!: SkillsDTO;
+  softskill!: softskillsDTO;
+  tituloModal: string = "";
+  Edit: boolean = false; 
+
+  isSoft: boolean = false;
+  isSkill: boolean = false;
+
+  constructor(private portfolioServ : PortfolioService){}
+  
+  ngOnInit(): void {
+  }
+
+  abrirModalAgregar() : void {
+    if (this.isSoft){
+      let soft = {id:0,userId:this.userId,skillName:"",icon:""};
+      this.softskill = soft;
+      this.tituloModal = "Agregar elemento a Habilidades Blandas";
+      this.Edit = true; 
+    } else {
+      let skil = {skillId:0,userId:this.userId,nivelId:0, skillName:"",proficiencyLevel:"",icon:""};
+      this.skill = skil;
+      this.tituloModal = "Agregar elemento a Habilidades Técnicas";
+      this.Edit = true;
+    }
+  }
+
+  abrirEditarHabTecnica(skil: SkillsDTO): void {
+    this.skill = skil;
+    this.tituloModal = "Editar elemento en Habilidades Técnicas";
+    this.Edit = true;    
+  }
+
+  deleteSkill(skillId: any): void {
+    if(skillId != undefined && confirm("¿Estás segura de querer eliminar este elemento?")){
+      this.portfolioServ.deleteSkill(skillId).subscribe(data => {
+        alert("El elemento ha sido eliminado");
+        this.recargandoPortfolio.emit();
+        //this.listarPorNIvel();
+        window.location.reload();
+        });
+    }
+  }
+
+  abrirEditarHabBlanda(soft: softskillsDTO): void {
+    this.softskill = soft;
+    this.tituloModal = "Editar elemento en Habilidades Blandas";
+    this.Edit = true;
+  }
+
+  deleteSoftSkill(softId: any): void {
+    if(softId != undefined && confirm("¿Estás segura de querer eliminar este elemento?")){
+      this.portfolioServ.deleteSoftSkill(softId).subscribe(data => {
+        alert("El elemento ha sido eliminado");
+        this.recargandoPortfolio.emit();
+        window.location.reload();
+        });
+    }
+  }
+
+  cerrarModal(): void {
+    this.isSoft = false;
+    this.Edit = false;
+    this.isSkill = false;
+
+    //recarga la vista después de cerrar modal agregar o editar
+    window.location.reload();
+  }
 }
